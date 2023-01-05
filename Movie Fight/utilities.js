9e87f8a29446ffcd925379b7dd1fb9599664b62b
szabodaniel995 <121961543+szabodaniel995@users.noreply.github.com>
2023-01-05 13:34:30 +0100

    Add files via upload

---
+++ Movie Fight/utilities.js
@@ -0,0 +1,12 @@
+
+function debounce(func, delay=1000) {
+    let id;
+    return () => {
+        if (id) {
+            clearTimeout(id);
+        }
+        id = setTimeout(() => {
+            func();
+        }, delay);
+    }  
+};  
